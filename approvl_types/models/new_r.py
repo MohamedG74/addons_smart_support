4450adc2690432fef6d16ba56989c7ae75e5ef99
MohamedG74 <mohammed20191700519@cis.asu.edu.eg>
2023-10-11 12:34:53 +0300

    Task 1

--- approvl_types/models/new_r.py
+++ approvl_types/models/new_r.py
@@ -3,13 +3,15 @@ from odoo import api,fields,models
 class Newr(models.Model):
     _inherit='approval.request'
 
-    test2 = fields.Selection(related="category_id.test1")
-    test3=fields.Char(string='Test')
+    # test2 = fields.Selection(related="category_id.test1")
+    # #test3=fields.Char(string='Test')
+    subj_id=fields.Text(string="Subject")
 
 class Newf(models.Model):
     _inherit='approval.category'
 
-    test1=fields.Selection([('required', 'Required'),('optional','Optional'),('no','None')],string='Test')
+    #test1=fields.Selection([('required', 'Required'),('optional','Optional'),('no','None')],string='Test')
+   # subj_ids=fields.Selection([('required', 'Required'),('optional','Optional'),('no','None')],string='Test')
 
 
 class New_line(models.Model):
