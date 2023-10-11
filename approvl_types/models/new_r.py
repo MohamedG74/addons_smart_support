aaddfe2c7131a4d9b2abd6aad30e17d86e7e97e5
MohamedG74 <mohammed20191700519@cis.asu.edu.eg>
2023-10-11 11:27:42 +0300

    new task

--- approvl_types/models/new_r.py
+++ approvl_types/models/new_r.py
@@ -10,3 +10,17 @@ class Newf(models.Model):
     _inherit='approval.category'
 
     test1=fields.Selection([('required', 'Required'),('optional','Optional'),('no','None')],string='Test')
+
+
+class New_line(models.Model):
+        _inherit='approval.product.line'
+
+        cate_ids_line=fields.Many2one('product.category',string="Category line",related="product_id.categ_id")
+        type_ids_line=fields.Selection(string="Type",related="product_id.detailed_type")
+        # @api.depends('product_id')
+        # def _compute_category(self):
+        #     for rec in self:
+        #         if rec.product_id:
+        #             rec.cate_ids_line=rec.product_id.categ_id
+        #         else:
+        #             rec.cate_ids_line = False
