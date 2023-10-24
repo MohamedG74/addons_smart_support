035e9c2b3e58bfe209d46ae72533f686510da5fc
Mohamed Gamal Tawfek <72981062+MohamedG74@users.noreply.github.com>
2023-10-24 17:15:30 +0300

    updates

--- purchase_modify/models/changes.py
+++ purchase_modify/models/changes.py
@@ -1,39 +1,12 @@
 from odoo import api,models,fields
-from odoo.exceptions import ValidationError
-class ModifyProduct(models.Model):
-    _inherit='product.product'
 
-    brand=fields.Char(string="Brand")
-    model_number=fields.Char(string="Model Number")
 class ModifyPurchase(models.Model):
     _inherit='purchase.order.line'
-    # brand_ids=fields.Char(string='Brand',related='product_id.brand')
-    # model_num_ids=fields.Char(string='Model Number',related='product_id.model_number')
-    disc_ids=fields.Float(string="Discount %")
-    untaxed_ids=fields.Float(string="Untaxed Amount",default='0.0',compute='_compute_untax')
-    disc_amount_ids=fields.Float(string="Discount Amount",compute='_compute_disc_amount')
-    custom_ids=fields.Float(string="Test",compute='_compute_total')
-    # status_ids=fields.Selection([('non urgent','Non Urgent'),('urgent','Urgent'),('semi urgent','Semi Urgent'),('emerg','Emergency')])
-
-    # @api.depends('disc_ids')
-    # def _compute_disc(self):
-    #     for rec in self:
-    #         rec.disc_ids=rec.disc_ids/100
-
-
-    @api.depends('product_qty','disc_ids','price_unit')
-    def _compute_total(self):
-        self.disc_ids=self.disc_ids/100
+    disc_ids=fields.Float(string="Discount %")    
+    
+    @api.onchange('disc_ids')
+    def onchange_price(self):
         for rec in self:
-           
-            rec.custom_ids=rec.product_qty*rec.price_unit*rec.disc_ids
-            rec.price_subtotal=rec.price_subtotal-rec.custom_ids
-
-    @api.depends('price_subtotal')
-    def _compute_untax(self):
-        for rec in self:
-            rec.untaxed_ids=rec.price_subtotal
-    @api.depends('disc_ids')
-    def _compute_disc_amount(self):
-        for rec in self:
-            rec.disc_amount_ids=rec.price_unit*rec.product_qty*rec.disc_ids
+            rec.price_unit=rec.price_unit*(rec.disc_ids/100)
+            rec.price_subtotal=rec.product_qty*rec.price_unit*rec.disc_ids
+            
