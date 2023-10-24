44bfd14342ec0632678fe8de26857aa449eae0b9
Mohamed Gamal Tawfek <72981062+MohamedG74@users.noreply.github.com>
2023-10-24 15:37:48 +0300

    modify

--- purchase_modify/models/changes.py
+++ purchase_modify/models/changes.py
@@ -7,13 +7,33 @@ class ModifyProduct(models.Model):
     model_number=fields.Char(string="Model Number")
 class ModifyPurchase(models.Model):
     _inherit='purchase.order.line'
-    brand_ids=fields.Char(string='Brand',related='product_id.brand')
-    model_num_ids=fields.Char(string='Model Number',related='product_id.model_number')
-    cbm_ids=fields.Float(string="CBM",default='0.0')
-    status_ids=fields.Selection([('non urgent','Non Urgent'),('urgent','Urgent'),('semi urgent','Semi Urgent'),('emerg','Emergency')])
+    # brand_ids=fields.Char(string='Brand',related='product_id.brand')
+    # model_num_ids=fields.Char(string='Model Number',related='product_id.model_number')
+    disc_ids=fields.Float(string="Discount %")
+    untaxed_ids=fields.Float(string="Untaxed Amount",default='0.0',compute='_compute_untax')
+    disc_amount_ids=fields.Float(string="Discount Amount",compute='_compute_disc_amount')
+    custom_ids=fields.Float(string="Test",compute='_compute_total')
+    # status_ids=fields.Selection([('non urgent','Non Urgent'),('urgent','Urgent'),('semi urgent','Semi Urgent'),('emerg','Emergency')])
 
-    @api.constrains('status_ids')
-    def _check_status(self):
+    # @api.depends('disc_ids')
+    # def _compute_disc(self):
+    #     for rec in self:
+    #         rec.disc_ids=rec.disc_ids/100
+
+
+    @api.depends('product_qty','disc_ids','price_unit')
+    def _compute_total(self):
+        self.disc_ids=self.disc_ids/100
         for rec in self:
-            if rec.status_ids=='emerg':
-                raise ValidationError(str(rec.product_id) +'is Emergency')
+           
+            rec.custom_ids=rec.product_qty*rec.price_unit*rec.disc_ids
+            rec.price_subtotal=rec.price_subtotal-rec.custom_ids
+
+    @api.depends('price_subtotal')
+    def _compute_untax(self):
+        for rec in self:
+            rec.untaxed_ids=rec.price_subtotal
+    @api.depends('disc_ids')
+    def _compute_disc_amount(self):
+        for rec in self:
+            rec.disc_amount_ids=rec.price_unit*rec.product_qty*rec.disc_ids
