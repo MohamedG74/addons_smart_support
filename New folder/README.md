2549e697c5f6fa132508783adadc439bd33f397a
Mohamed Gamal Tawfek <72981062+MohamedG74@users.noreply.github.com>
2023-11-24 20:14:06 +0200

    nodjs adminlte

---
+++ New folder/README.md
@@ -0,0 +1,39 @@
+# What is it? 
+
+This project can be seen as a starting template to send some endpoints that are managed with a backoffice.
+
+It was built with **NodeJS**, **ExpressJS**, **Bootstrap 4**, **AdminLTE 3**, **Pug** and **MySQL**. 
+
+## Features and ToDo List
+
+* [x] Add, Edit and Delete data from DB with the use of the backoffice
+* [x] Login and Logout
+* [x] Endpoint to send data from the DB 
+* [x] Responsive data tables
+* [x] Pagination and search from data tables
+* [x] Export options: CSV, PDF e Print
+* [x] Dynamic menu
+* [ ] Upload image example
+* [ ] Manage login users in backoffice
+* [ ] Store session for a period of time (Remember me)
+* [ ] Forgot password form
+* [ ] Register form
+
+## How to use it? 
+
+After cloning this repo, run the command: ```npm install```
+
+Import the database that is inside the folder **db**
+
+Copy ```.env.example``` to ```.env``` and add your settings, including database connection
+
+To run the project, run: ```npm start```
+
+The default port is ```:3000``` and you can use ```teste@teste.pt``` and ```teste``` as password to login.
+
+The example endpoint can be accessed in ```http://localhost:3000/api/customers```
+
+### Links
+
+[AdminLte](https://adminlte.io/)
+[Pug - How to use](https://flaviocopes.com/pug/#including-other-pug-files)
---
+++ New folder/app.js
@@ -0,0 +1,89 @@
+require('dotenv').config();
+
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const flash = require('express-flash');
+const session = require('express-session');
+const index = require('./routes/index');
+const users = require('./routes/users');
+const customers = require('./routes/customers');
+const api = require('./routes/api');
+const invoicesearch = require('./routes/invoice_search');
+
+
+const expressValidator = require('express-validator');
+const methodOverride = require('method-override');
+const connection = require('express-myconnection');
+const mysql = require('mysql2');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({ secret: "secretpass123456" }));
+app.use(flash());
+app.use(expressValidator());
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body == 'object' && '_method' in req.body) {
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+  connection(mysql, {
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASS,
+    port: process.env.DBPORT,
+    database: process.env.DBNAME,
+  }, 'pool') //single, pool, request
+);
+
+app.use('/', index);
+app.use('/customers', customers);
+app.use('/users', users);
+app.use('/api', api);
+app.use('/invoice_search', invoicesearch);
+
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+module.exports = app;
